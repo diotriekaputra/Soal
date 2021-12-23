6975f6fff616f9253688ffde1f83cffb9eebf92f
diotriekaputra <diotriekaputra97@gmail.com>
2021-12-23 11:47:53 +0700

    Update Client Startup

---
+++ Soal 3/CarRental/CarRentalClient/Controllers/EmployeesRentalController.cs
@@ -0,0 +1,49 @@
+﻿using CarRental.Models;
+using CarRental.ViewModel;
+using LeaveClient.Base.Controllers;
+using LeaveClient.Repository.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalClient.Controllers
+{
+    public class EmployeesRentalController : BaseController<Employee, EmployeeRentalRepository, string>
+    {
+        private readonly EmployeeRentalRepository repository;
+        public EmployeesRentalController(EmployeeRentalRepository repository) : base(repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Auth(LoginVM login)
+        {
+            var jwtToken = await repository.Auth(login);
+            var token = jwtToken.Token;
+            var pesan = jwtToken.Messages;
+            var empId = jwtToken.NIK;
+
+            if (token == "" || token == null)
+            {
+                if (pesan == "0")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            HttpContext.Session.SetString("JWToken", token);
+            return RedirectToAction("Index", "Auth");
+        }
+    }
+}
